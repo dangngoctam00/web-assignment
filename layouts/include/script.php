24211a918d3f6307ed9120e7b6c0578d013e2415
dangngoctam00 <dangngoctam00@gmail.com>
2021-04-21 23:10:16 +0700

    initial commit

---
+++ layouts/include/script.php
@@ -0,0 +1,2 @@
+<script src="/assets/public/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/public/js/jquery.min.js"></script>
---
+++ layouts/include/stylesheet.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="/assets/css/common_styles.css">
+<link rel="stylesheet" href="/assets/public/css/bootstrap.min.css">
