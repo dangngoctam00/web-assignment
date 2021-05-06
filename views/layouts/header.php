0eea2642c20c7e606fe89e9a59dedb0e2473b55d
dangngoctam00 <dangngoctam00@gmail.com>
2021-05-06 23:07:13 +0700

    fix path

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,9 +1,9 @@
-<link rel="stylesheet" href="/assets/css/header.css">
+<link rel="stylesheet" href="../../assets/css/header.css">
 <header class="header">
 
   <div class="container-fluid">
     <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <a class="navbar-brand" href="#"><img src="/assets//images/logo.png" alt="" class="logo"></a>
+      <a class="navbar-brand" href="#"><img src="../../assets/images/logo.png" alt="" class="logo"></a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
