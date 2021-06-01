bc619e850d34354c9eb39833f6928aec3c364afa
bathongkt123 <bathongkt123@gmail.com>
2021-06-01 17:51:43 +0700

    Push database

---
+++ data/bookstore.sql
@@ -0,0 +1,86 @@
+drop SCHEMA if EXISTS bookstore;
+create SCHEMA bookstore;
+use bookstore;
+CREATE TABLE customer (
+    id int AUTO_INCREMENT,
+    name varchar(40),
+    email varchar(50),
+    phone varchar(15),
+    birthdate date,
+    registered_at datetime,
+    PRIMARY KEY (id)
+);
+create TABLE admin(
+    id int AUTO_INCREMENT,
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    user_name varchar(30) UNIQUE,
+    email varchar(50),
+    phone varchar(15),
+    birthdate date,
+    registered_at datetime,
+    PRIMARY KEY (id)
+);
+create TABLE employee(
+    id int AUTO_INCREMENT,
+    full_name varchar(40),
+    work_as varchar(20),
+    link_image varchar(100),
+    link_twitter varchar(100),
+    link_instagram varchar(100),
+    PRIMARY KEY (id)
+);
+create TABLE book(
+	id int AUTO_INCREMENT,
+    name varchar(20) not NULL,
+    category varchar(20) not null,
+    price int not null,
+    link_description varchar(50),
+    link_image varchar(50),
+    PRIMARY KEY (id)
+);
+#One book may have more than 1 author.
+CREATE TABLE written_by (
+    book_id int,
+    author varchar(20),
+    PRIMARY key (book_id,author),
+    FOREIGN key (book_id) REFERENCES book(id)
+);
+CREATE table reviewed_by(
+    book_id int,
+    customer_id int,
+    #Number of stars 0-5 quality and price
+    quality int,
+    price int,
+    PRIMARY KEY (book_id, customer_id),
+    FOREIGN KEY (book_id) REFERENCES book(id),
+    FOREIGN key (customer_id) REFERENCES customer(id)
+);
+CREATE TABLE shopping_log(
+    id int AUTO_INCREMENT,
+    customer_id int,
+    total_price int,
+    created_at datetime,
+    PRIMARY KEY (id),
+    FOREIGN key (customer_id) REFERENCES customer(id)
+);
+#shopping log contains books and numbers of each of book.
+create table shopping_log_entry(
+    log_id int,
+    book_id int,
+    quantity int,
+    PRIMARY key(log_id, book_id),
+    FOREIGN key (log_id) REFERENCES shopping_log(id),
+    FOREIGN key (book_id) REFERENCES book(id)
+);
+create table send_email_log(
+    id int AUTO_INCREMENT,
+    first_name varchar(20),
+    last_name varchar(20),
+    email varchar(30),
+    website varchar(50),
+    subject varchar(100),
+    message varchar(255),
+    created_at datetime,
+    PRIMARY KEY (id)
+);
