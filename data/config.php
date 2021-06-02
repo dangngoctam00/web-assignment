91fbdd01ea0962b67bdbb826d3b8786f8a0bc7d3
dangngoctam00 <dangngoctam00@gmail.com>
2021-06-02 13:04:20 +0700

    fix database

---
+++ data/config.php
@@ -0,0 +1,13 @@
+<?php
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', '___');
+	define('DB_PASSWORD', '___');
+	define('DB_NAME', 'bookstore');
+
+	// Attempt to connect to MySQL database
+	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+	if (!$mysql_db) {
+		die("Error: Unable to connect " . $mysql_db->connect_error);
+	}
+?>
